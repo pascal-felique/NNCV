e2a4f1e2e229a96e68c7189a37f2ee5e7001438a
pascal-felique <pascal_felique@hotmail.com>
2025-04-18 19:43:07 +0200

    Generic name for model to be tested

--- Final assignment/main_efficiency.sh
+++ Final assignment/main_efficiency.sh
@@ -2,4 +2,4 @@ python3 evaluate_efficiency.py \
     --data-dir ./data/cityscapes \
     --batch-size 32 \
     --num-workers 8 \
-    --model-path ./models/fastscnn-training-experiment-01.pth \
+    --model-path ./models/model.pth \
