834384f98a883cf20bde048b299a54455af674cb
pascal-felique <pascal_felique@hotmail.com>
2025-04-19 13:55:54 +0200

    Change experiment id

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,4 +7,4 @@ python3 train.py \
     --lr 0.001 \
     --num-workers 8 \
     --seed 42 \
-    --experiment-id "fastscnn-training-experiment-02" \
+    --experiment-id "fastscnn-training-experiment-01" \
