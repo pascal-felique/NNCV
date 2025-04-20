3ededc7dc8f72f593988aed7781d16ff7119cbc5
pascal-felique <pascal_felique@hotmail.com>
2025-04-20 17:43:21 +0200

    Increase experiment id and update previous trained model path

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,5 +7,5 @@ python3 train.py \
     --lr 0.001 \
     --num-workers 8 \
     --seed 42 \
-    --experiment-id "fastscnn-training-experiment-03" \
-    --model-path "./models/fastscnn-training-experiment-02.pth" \
+    --experiment-id "fastscnn-training-experiment-04" \
+    --model-path "./models/fastscnn-training-experiment-03.pth" \
--- Final assignment/train.py
+++ Final assignment/train.py
@@ -73,10 +73,10 @@ patch_height5=768
 ####################################
 # Select image size and patch size
 ####################################
-resized_image_width=resized_image_width3
-resized_image_height=resized_image_height3
-patch_width=patch_width3
-patch_height=patch_height3
+resized_image_width=resized_image_width4
+resized_image_height=resized_image_height4
+patch_width=patch_width4
+patch_height=patch_height4
 
 # Function to calculate mean and standard deviation
 def calculate_mean_std(dataloader):
