c932b09340af52dc419d837cf7017485875dd947
pascal-felique <pascal_felique@hotmail.com>
2025-04-25 15:23:43 +0200

    Add support for knowledge distillation

---
+++ Final assignment/main_distillation.sh
@@ -0,0 +1,14 @@
+wandb login
+
+python3 train.py \
+    --data-dir "./data/cityscapes" \
+    --batch-size 32 \
+    --epochs 100 \
+    --lr 0.001 \
+    --num-workers 8 \
+    --seed 42 \
+    --experiment-id "fastscnn-training-experiment-11" \
+    --trained-teacher-model-path "./models/fastscnn-training-experiment-05.pth" \
+    --previous-student-model-path "none" \
+    --alpha 0.5 \
+    --temperature 4.0 \
