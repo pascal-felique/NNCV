a3d9be864ba63e428ea9276db97843b584bcd613
pascal-felique <pascal_felique@hotmail.com>
2025-04-26 11:17:10 +0200

    Increase experiment id and update previous trained model and increase resolution for images and patches

--- Final assignment/train_distillation.py
+++ Final assignment/train_distillation.py
@@ -74,10 +74,10 @@ patch_height5=768
 ####################################
 # Select image size and patch size
 ####################################
-resized_image_width=resized_image_width4
-resized_image_height=resized_image_height4
-patch_width=patch_width4
-patch_height=patch_height4
+resized_image_width=resized_image_width5
+resized_image_height=resized_image_height5
+patch_width=patch_width5
+patch_height=patch_height5
 
 def distillation_loss(student_logits, teacher_logits, labels, T, alpha):
     kd_loss = F.kl_div(
