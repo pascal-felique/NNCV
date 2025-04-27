77bd0dcb48e592a5e09131a3fe92b9617c5c863a
pascal-felique <pascal_felique@hotmail.com>
2025-04-27 10:23:51 +0200

    Increase experiment id and update previous model path and increase resolution of images and patches

--- Final assignment/train_dice.py
+++ Final assignment/train_dice.py
@@ -74,10 +74,10 @@ patch_height5=768
 ####################################
 # Select image size and patch size
 ####################################
-resized_image_width=resized_image_width2
-resized_image_height=resized_image_height2
-patch_width=patch_width2
-patch_height=patch_height2
+resized_image_width=resized_image_width3
+resized_image_height=resized_image_height3
+patch_width=patch_width3
+patch_height=patch_height3
 
 def dice_loss(predictions, targets, epsilon=1e-6):
     """
