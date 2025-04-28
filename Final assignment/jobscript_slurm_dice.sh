ce66c7507d663a118216b920060f4c46a1430df3
pascal-felique <pascal_felique@hotmail.com>
2025-04-28 16:36:28 +0200

    Reduce time slot on super cluster

--- Final assignment/jobscript_slurm_dice.sh
+++ Final assignment/jobscript_slurm_dice.sh
@@ -4,6 +4,6 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_h100
-#SBATCH --time=24:00:00
+#SBATCH --time=15:00:00
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main_dice.sh
